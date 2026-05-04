849fe3b15f15d78dc4f8850d65e6457d73909fd3
pbfluffy <145362755+pbfluffy@users.noreply.github.com>
2026-05-04 18:00:06 +0700

    Update base path in Vite configuration

--- bill-splitter/vite.config.js
+++ bill-splitter/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/LesProjets/bill-splitter/',
+  base: '/bill-splitter/', // 👈 MUST match your subpath exactly
 })
