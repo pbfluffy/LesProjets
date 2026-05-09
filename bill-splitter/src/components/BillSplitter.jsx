b18dfc344a46e59f7c5ca2c05aab21d67ce5f22d
pbfluffy <145362755+pbfluffy@users.noreply.github.com>
2026-05-09 18:01:42 +0700

    BillSplitter: shared state + readOnly fieldset + snapshot

--- bill-splitter/src/components/BillSplitter.jsx
+++ bill-splitter/src/components/BillSplitter.jsx
@@ -4,15 +4,28 @@ import ExtrasSection from './ExtrasSection'
 import ResultSection from './ResultSection'
 import { useBillStore } from '../hooks/useBillStore'
 
-export default function BillSplitter() {
-  const store = useBillStore()
+const fieldsetReset = { border: 0, padding: 0, margin: 0, minInlineSize: 'auto' }
+
+export default function BillSplitter({ sharedState, readOnly }) {
+  const store = useBillStore(sharedState)
   const result = store.calculate()
+  const snapshot = {
+    members: store.members,
+    foods: store.foods,
+    vatEnabled: store.vatEnabled,
+    serviceChargeEnabled: store.serviceChargeEnabled,
+    promptPay: store.promptPay,
+    bankInfo: store.bankInfo,
+    notes: store.notes,
+  }
   return (
     <div>
-      <MemberSection members={store.members} onAdd={store.addMember} onRemove={store.removeMember} />
-      <FoodList foods={store.foods} members={store.members} onAdd={store.addFood} onUpdate={store.updateFood} onToggleMember={store.toggleFoodMember} onRemove={store.removeFood} onSelectAll={store.setAllMembers} />
-      <ExtrasSection vatEnabled={store.vatEnabled} onVatChange={store.setVatEnabled} serviceChargeEnabled={store.serviceChargeEnabled} onServiceChargeChange={store.setServiceChargeEnabled} promptPay={store.promptPay} onPromptPayChange={store.setPromptPay} bankInfo={store.bankInfo} onBankInfoChange={store.setBankInfo} notes={store.notes} onNotesChange={store.setNotes} />
-      <ResultSection result={result} members={store.members} promptPay={store.promptPay} bankInfo={store.bankInfo} notes={store.notes} />
+      <fieldset disabled={readOnly} style={fieldsetReset}>
+        <MemberSection members={store.members} onAdd={store.addMember} onRemove={store.removeMember} />
+        <FoodList foods={store.foods} members={store.members} onAdd={store.addFood} onUpdate={store.updateFood} onToggleMember={store.toggleFoodMember} onRemove={store.removeFood} onSelectAll={store.setAllMembers} />
+        <ExtrasSection vatEnabled={store.vatEnabled} onVatChange={store.setVatEnabled} serviceChargeEnabled={store.serviceChargeEnabled} onServiceChargeChange={store.setServiceChargeEnabled} promptPay={store.promptPay} onPromptPayChange={store.setPromptPay} bankInfo={store.bankInfo} onBankInfoChange={store.setBankInfo} notes={store.notes} onNotesChange={store.setNotes} />
+      </fieldset>
+      <ResultSection result={result} members={store.members} promptPay={store.promptPay} bankInfo={store.bankInfo} notes={store.notes} snapshot={snapshot} tab="split" />
     </div>
   )
 }
