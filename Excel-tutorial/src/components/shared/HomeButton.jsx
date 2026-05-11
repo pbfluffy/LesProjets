ef43a1fefbceaac0569999e4fe3077fe5a47bb2f
pbfluffy <145362755+pbfluffy@users.noreply.github.com>
2026-05-11 09:54:04 +0700

    Delete Excel-tutorial directory

--- Excel-tutorial/src/components/shared/HomeButton.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './HomeButton.module.css'
-
-export default function HomeButton({ href = '../', label = '← Home' }) {
-  return (
-    <a href={href} className={styles.btn} title="Home">
-      {label}
-    </a>
-  )
-}
