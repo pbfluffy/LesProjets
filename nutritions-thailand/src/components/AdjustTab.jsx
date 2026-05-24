70de1cb24caee4154d8bbfc61b8bd4a17a394b36
pbfluffy <145362755+pbfluffy@users.noreply.github.com>
2026-05-24 13:04:10 +0700

    Nutritions: remove WaterReminderCard and SyncCard (overkill/redundant)

--- nutritions-thailand/src/components/AdjustTab.jsx
+++ nutritions-thailand/src/components/AdjustTab.jsx
@@ -9,9 +9,6 @@ import {
 } from '../data/constants.js';
 import DataPanel from './DataPanel.jsx';
 import styles from './AdjustTab.module.css';
-
-import WaterReminderCard from './WaterReminderCard.jsx';
-import SyncCard from './SyncCard.jsx';
 export default function AdjustTab({ store }) {
   const { t } = useLang();
   const { stats, setStat } = store;
@@ -265,8 +262,6 @@ export default function AdjustTab({ store }) {
         )}
       </div>
 
-      <WaterReminderCard store={store} />
-      <SyncCard store={store} />
       <DataPanel store={store} />
     </>
   
