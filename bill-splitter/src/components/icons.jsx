0d4c299b8ab93e4d8854922f8d43aeff5e8f1c4c
pbfluffy <145362755+pbfluffy@users.noreply.github.com>
2026-05-28 20:28:20 +0700

    Update icons.jsx

--- bill-splitter/src/components/icons.jsx
+++ bill-splitter/src/components/icons.jsx
@@ -49,3 +49,22 @@ export function SunIcon() {
     </svg>
   )
 }
+
+export function CopyIcon(props) {
+  return (
+    <svg {...base} {...props}>
+      <rect x="9" y="9" width="13" height="13" rx="2" />
+      <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" />
+    </svg>
+  )
+}
+
+export function ShareIcon(props) {
+  return (
+    <svg {...base} {...props}>
+      <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8" />
+      <path d="M16 6l-4-4-4 4" />
+      <path d="M12 2v13" />
+    </svg>
+  )
+}
