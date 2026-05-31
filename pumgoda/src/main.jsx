cf83ccf0e752ebb0128b6d5da5cd2912bff5e5ce
pbfluffy <145362755+pbfluffy@users.noreply.github.com>
2026-05-31 16:16:54 +0700

    Update main.jsx

--- pumgoda/src/main.jsx
+++ pumgoda/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '../../shared/theme-tokens.css'
 import App from './App.jsx'
 import './App.css'
 import './registerPwaUpdate.js'
